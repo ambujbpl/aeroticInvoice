529bfcb6ec94d1337ddd106e54e2c547c6e85a4e
harshitadubey <harshitadubeybpl@gmail.com>
2020-02-29 08:27:08 +0530

    registration work done

--- old_web/registration.php
+++ old_web/registration.php
@@ -31,7 +31,7 @@
     			<div class="row">
     				<div class="col-xs-6 col-sm-6 col-md-6">
     					<div class="form-group">
-                			<input type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
+                	<input type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
     					</div>
     				</div>
     				<div class="col-xs-6 col-sm-6 col-md-6">
