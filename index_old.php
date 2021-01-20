e260642d9704c29f7e67fe8d0e49039b742d1c13
harshitadubey <harshitadubeybpl@gmail.com>
2021-01-21 00:35:48 +0530

    new index page contains details of the product

---
+++ index_old.php
@@ -0,0 +1,15 @@
+<?php
+  session_start();
+	if(isset($_SESSION["user_id"])) {
+		include('./web/php/function.php');
+		if(!isLoginSessionExpired()) {
+			echo "<script> localStorage.clear(); </script>";
+	    header("Location: web/login.html");
+		} else {
+			header("Location: web/dashboard.html");
+		}
+	}else{
+		echo "<script> localStorage.clear(); </script>";
+	  header("Location: web/login.html");
+	}
+?>
