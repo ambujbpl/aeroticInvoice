63f59d5d36f2199257a65c2e15cf6c625779af75
ambujbpl <ambujdubey89@gmail.com>
2022-02-16 07:09:03 +0530

    work in progress for user

--- web/js/main/dashboard.js
+++ web/js/main/dashboard.js
@@ -1,19 +1,30 @@
 var user = localStorage.getItem('user');
-if(user){
+if (user) {
 	user = JSON.parse(user);
-  if(user.role.toLowerCase() != "admin") {
-    $.notify('Sorry, You do not have admin access','error');
-    $('.adminAccess').addClass('hide');
-  }
-}else{
+	if (user.role.toLowerCase() != "admin") {
+		$.notify('Sorry, You do not have admin access', 'error');
+		// $('.adminAccess').addClass('hide');
+	}
+	// Upcomming functionality
+	// renderSideBarByUserRole(user?.role?.toLowerCase())
+} else {
 	logoutFunction();
 }
-$(document).ready(function() {
+
+// // Upcomming functionality
+// const renderSideBarByUserRole = (role) => {
+// 	let html = '';
+// 	if(['admin'].includes(role)){
+// 		html += ``
+// 	}
+// } 
+
+$(document).ready(function () {
 	$(".userName").html(user.first_name);
-	if(user.profile_file_name && user.profile_file_name  !=undefined && user.profile_file_name != ""){
-		$(".img-profile").attr("src","profilePic/" + user.profile_file_name);
+	if (user.profile_file_name && user.profile_file_name != undefined && user.profile_file_name != "") {
+		$(".img-profile").attr("src", "profilePic/" + user.profile_file_name);
 	} else {
-		$(".img-profile").attr("src","profilePic/defualt.png");
+		$(".img-profile").attr("src", "profilePic/defualt.png");
 	}
 	updateCounters();
 });
@@ -26,7 +37,7 @@ createBill = () => {
 	$('.mainContainerHeader').html('Create Bill');
 	$('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Create Bill Header:</div>`).append(`<a class="dropdown-item" onclick="showAddBill();">Create a Bill</a>`).append(`<a class="dropdown-item" onclick="viewBill();">View Bill</a>`);
 	// executeCustomQuery("select * from basic","mainContainerBody","basicDetails");
-	showAddBill();	
+	showAddBill();
 }
 
 /**
@@ -37,7 +48,18 @@ createPUC = () => {
 	$('.mainContainerHeader').html('Create PUC');
 	$('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Create PUC Header:</div>`).append(`<a class="dropdown-item" onclick="showAddPUC();">Create a PUC</a>`).append(`<a class="dropdown-item" onclick="viewPUC();">View PUC</a>`);
 	// executeCustomQuery("select * from basic","mainContainerBody","basicDetails");
-	showAddPUC();	
+	showAddPUC();
+}
+
+/**
+ * { Create User }
+ */
+createUser = () => {
+	$('.mainContainerRow').removeClass('hide');
+	$('.mainContainerHeader').html('Create User');
+	$('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Create User Header:</div>`).append(`<a class="dropdown-item" onclick="showAddUser();">Create a User</a>`).append(`<a class="dropdown-item" onclick="viewUser();">View User</a>`);
+	// executeCustomQuery("select * from basic","mainContainerBody","basicDetails");
+	showAddUser();
 }
 
 /**
@@ -61,7 +83,7 @@ showAddPUC = () => {
 viewBill = () => {
 	$('.mainContainerRow').removeClass('hide');
 	$('.mainContainerHeader').html('View Bill Details');
-	showServerSideDetailsByTableNameDataTable("invoices","mainContainerBody","invoicesDetails");
+	showServerSideDetailsByTableNameDataTable("invoices", "mainContainerBody", "invoicesDetails");
 }
 
 /**
@@ -70,7 +92,16 @@ viewBill = () => {
 viewPUC = () => {
 	$('.mainContainerRow').removeClass('hide');
 	$('.mainContainerHeader').html('View PUC Details');
-	showServerSideDetailsByTableNameDataTable("certificates","mainContainerBody","pucDetails");
+	showServerSideDetailsByTableNameDataTable("certificates", "mainContainerBody", "pucDetails");
+}
+
+/**
+ * { view the User }
+ */
+viewUser = () => {
+	$('.mainContainerRow').removeClass('hide');
+	$('.mainContainerHeader').html('View User Details');
+	showServerSideDetailsByTableNameDataTable("user", "mainContainerBody", "pucDetails");
 }
 /**
  * { basic Details }
@@ -79,7 +110,7 @@ basicDetails = () => {
 	$('.mainContainerRow').removeClass('hide');
 	$('.mainContainerHeader').html('Basic Details');
 	$('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Basic Details Header:</div>`).append(`<a class="dropdown-item" onclick="basicDetails();">Basic Detail View</a>`).append(`<a class="dropdown-item" onclick="editMainContainerBody();">Edit Basic Detail</a>`);
-	executeCustomQuery("select * from basic","mainContainerBody","basicDetails");
+	executeCustomQuery("select * from basic", "mainContainerBody", "basicDetails");
 }
 
 /**
@@ -90,17 +121,17 @@ productDetails = () => {
 	$('.mainContainerHeader').html('Product Details');
 	$('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Product Details Header:</div>`).append(`<a class="dropdown-item" onclick="productDetails();">View Product Detail</a>`).append(`<a class="dropdown-item" onclick="addNewProduct('product');">Add New Product</a>`);
 	// executeCustomQuery("select * from product","mainContainerBody","productDetails");
-	showServerSideDetailsByTableNameDataTable("product","mainContainerBody","productDetails");
+	showServerSideDetailsByTableNameDataTable("product", "mainContainerBody", "productDetails");
 }
 /**
  * { reminders Details }
  */
 remindersDetails = () => {
-  $('.mainContainerRow').removeClass('hide');
-  $('.mainContainerHeader').html('Reminders Details');
-  $('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Reminders Details Header:</div>`).append(`<a class="dropdown-item" onclick="remindersDetails();">View Reminders Detail</a>`).append(`<a class="dropdown-item" onclick="addNewReminders('reminders');">Add New Reminders</a>`);
-  // executeCustomQuery("select * from product","mainContainerBody","reminders");
-  showServerSideDetailsByTableNameDataTable("reminders","mainContainerBody","remindersDetails");
+	$('.mainContainerRow').removeClass('hide');
+	$('.mainContainerHeader').html('Reminders Details');
+	$('.mainContainerDropdownMenuLink').html('').append(`<div class="dropdown-header">Dropdown Reminders Details Header:</div>`).append(`<a class="dropdown-item" onclick="remindersDetails();">View Reminders Detail</a>`).append(`<a class="dropdown-item" onclick="addNewReminders('reminders');">Add New Reminders</a>`);
+	// executeCustomQuery("select * from product","mainContainerBody","reminders");
+	showServerSideDetailsByTableNameDataTable("reminders", "mainContainerBody", "remindersDetails");
 }
 
 
@@ -108,8 +139,8 @@ remindersDetails = () => {
  * { update Counters }
  */
 updateCounters = () => {
-	executeCustomQuery("select count(*) as count from product","updatecounters","productCounts","");
-  executeCustomQuery("select count(*) as count from invoices","updatecounters","invoicesCounts","");
-  executeCustomQuery("select sum(paid) as count from invoices where (invoice_date between  DATE_FORMAT(NOW() ,'%Y-%m-01') AND NOW() )","updatecounters","sellCountMonthly","");
-  executeCustomQuery("select sum(paid) as count from invoices","updatecounters","sellCountAnnually","");
+	executeCustomQuery("select count(*) as count from product", "updatecounters", "productCounts", "");
+	executeCustomQuery("select count(*) as count from invoices", "updatecounters", "invoicesCounts", "");
+	executeCustomQuery("select sum(paid) as count from invoices where (invoice_date between  DATE_FORMAT(NOW() ,'%Y-%m-01') AND NOW() )", "updatecounters", "sellCountMonthly", "");
+	executeCustomQuery("select sum(paid) as count from invoices", "updatecounters", "sellCountAnnually", "");
 }
---
+++ web/js/main/userRoles.js
@@ -0,0 +1,6 @@
+const userRoles = [
+	{name : "Admin", value: "ADMIN"},
+	{name : "Viewer", value: "VIEWER"},
+	{name : "Puc Manager", value: "PUC"},
+	{name : "Bill Manager", value: "BILL"},
+]
